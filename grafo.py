aec0d3d6ceb9c6b2a047f23b49815d2c68f7a75c
Matheus Bento <matheus51bento@gmail.com>
2025-02-18 14:05:46 -0300

    initial

---
+++ grafo.py
@@ -0,0 +1,100 @@
+class Grafo:
+
+    def __init__(self):
+        self.head = None
+
+    class Vertice:
+        def __init__(self, data):
+            self.data = data
+            self.next = None
+            self.arestas = None
+
+    class Aresta:
+        def __init__(self, data):
+            self.data = data
+            self.next = None
+    
+    def add_vertice(self, data):
+        vertice = self.search(data)
+        if vertice is not None:
+            return None
+        if self.head is None:
+            self.head = self.Vertice(data)
+        else:
+            vertice = self.Vertice(data)
+            vertice.next = self.head
+            self.head = vertice
+
+    def add_aresta(self, vertice1, vertice2):
+        vertice = self.search(vertice1)
+        node_vertice = self.search(vertice2)
+        if vertice is None or node_vertice is None:
+            return None
+        
+        if vertice.arestas is None:
+            vertice.arestas = self.Aresta(node_vertice)
+        else:
+            aresta = self.Aresta(node_vertice)
+            aresta.next = vertice.arestas
+            vertice.arestas = aresta
+
+    def print(self, vertice: Vertice = None):
+        if vertice is None:
+            if self.head is None:
+                return None
+            vertice = self.head
+        
+        if vertice.arestas is not None:
+            string = self.print_arestas(vertice.arestas)
+            print(f"{vertice.data}: {string}")
+        else:
+            print(vertice.data)
+        if vertice.next is not None:
+            self.print(vertice.next)
+
+    def print_arestas(self, aresta: Aresta, string=""):
+        if aresta is None:
+            return string
+        string += f"-> {aresta.data.data} "
+        return self.print_arestas(aresta.next, string)
+
+    def search(self, data, vertice: Vertice = None):
+        if vertice is None:
+            if self.head is None:
+                return None
+            vertice = self.head
+        if vertice.data == data:
+            return vertice
+        if vertice.next is None:
+                return None
+        return self.search(data, vertice.next)
+        
+
+if __name__ == "__main__":
+    grafo = Grafo()
+    
+    try:
+        while True:
+            print("1 - Adicionar vertice")
+            print("2 - Adicionar aresta")
+            print("3 - Imprimir")
+            print("4 - Sair")
+            op = int(input("Escolha uma opção: "))
+            if op == 1:
+                data = input("Digite o dado do vertice: ")
+                grafo.add_vertice(data)
+            elif op == 2:
+                node1 = input("Digite o vertice de origem: ")
+                node2 = input("Digite o vertice de destino: ")
+                grafo.add_aresta(node1, node2)
+            elif op == 3:
+                print("\nGrafo:")
+                grafo.print()
+                print()
+            elif op == 4:
+                break
+            else:
+                print("Opção inválida")
+    except Exception as e:
+        print("Erro")
+        print(e)
